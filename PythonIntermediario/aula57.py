5721c0aa9ce3ae4e110ab494f2ba05aa461190fc
Pedro Neto <pedro.rmn19@gmail.com>
2023-12-28 15:16:55 -0300

    [Feat]: Arquivos de Aula

--- PythonIntermediario/aula57.py
+++ PythonIntermediario/aula57.py
@@ -1,5 +1,22 @@
 # Problema dos parâmetros mutáveis em funções Python
 
-def adiciona_clientes(nome, lista=[]):
+def adiciona_clientes(nome, lista=None):
+    if lista is None:
+        lista=[]
     lista.append(nome)
     return lista
+
+
+cliente1 = adiciona_clientes('Pedro')
+adiciona_clientes('Joana', cliente1)
+adiciona_clientes('Fernando', cliente1)
+
+cliente2 = adiciona_clientes('Helena')
+adiciona_clientes('Maria', cliente2)
+
+cliente3 = adiciona_clientes('Moreira')
+adiciona_clientes('Vivi', cliente3)
+
+print(cliente1)
+print(cliente2)
+print(cliente3)
